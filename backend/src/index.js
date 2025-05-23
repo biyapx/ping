6045d848691403cd847763d4330d83d2b7e9bb45
ab4io <ayalkbettesfahun@gmail.com>
2025-05-23 08:44:05 +0300

    one last time

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,21 @@
 import express from "express";
-import authRoutes from "./routes/route.auth..js";
-import messageRoutes from "./routes/message.route.js";
-import { connectDB } from "./lib/db.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
 
-dotenv.config();
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
-const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -19,8 +24,10 @@ app.use(
     credentials: true,
   })
 );
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -30,7 +37,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
