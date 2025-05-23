38ceda75ddb562e5beb16a6e62cd6ae95771fa78
ab4io <ayalkbettesfahun@gmail.com>
2025-05-23 04:15:08 +0300

    hmm

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,16 +2,17 @@ import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
 import cloudinary from "../lib/cloudinary.js";
 import { getRecieverSocketId, io } from "../lib/socket.js";
-export const getUsersForSideBar = async (req, res) => {
+export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
-    const filtersUsers = await User.find({
+    const filteredUsers = await User.find({
       _id: { $ne: loggedInUserId },
     }).select("-password");
-    res.status(200).json(filtersUsers);
+
+    res.status(200).json(filteredUsers);
   } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).json({ message: "Internal server error" });
+    console.error("Error in getUsersForSidebar: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
